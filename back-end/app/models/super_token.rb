d19d28a35da032095df45af16f57ad42890ee472
Mustafa Binalhag <mbin828@gmail.com>
2022-11-04 21:53:43 -0400

    removed old sms tokens staying valid till expiry

--- back-end/app/models/super_token.rb
+++ back-end/app/models/super_token.rb
@@ -3,15 +3,16 @@ class SuperToken < ApplicationRecord
     belongs_to :user
     # if no limit is desired then set const to 0
     # avoid changing this number but if changed all tokens prior to change will be deleted ordered by usage (the most active token stays)
-    LIMIT_TOKENS_PER_USER = 2
+    LIMIT_TOKENS_PER_USER = 2 # MINIMUN IS 2!!! sms tokens take 1 slot 
     AUTO_REFRESH = true
     DAYS_TO_EXPIRE = 14
+    SMS_EXPIRY = 10 # in minutes 
     def self.generate_token(user,request, sms=false)
         if user && request
             all_tokens = SuperToken.where(user_id: user.id)
             # if there is a limit destroy the least active token(s)
             if LIMIT_TOKENS_PER_USER != 0 && all_tokens.length >= LIMIT_TOKENS_PER_USER
-                if all_tokens.length > LIMIT_TOKENS_PER_USER
+                if all_tokens.length > LIMIT_TOKENS_PER_USER # this line should only happen in the event the const LIMIT changes
                     # Because the amount of active tokens exceeds the limit
                     # All of oldest tokens up to the limit must be deleted 
                     # This happens when the const LIMIT_TOKENS_PER_USER is changed from last start up
@@ -20,7 +21,7 @@ class SuperToken < ApplicationRecord
                     last_valid_time = all_tokens.order("updated_at DESC").slice(0,LIMIT_TOKENS_PER_USER-1).last.updated_at
                     all_tokens.where("updated_at < ?", last_valid_time).destroy_all
                 else
-                    all_tokens.order("updated_at")[0].destroy
+                    all_tokens.order("updated_at")[0].destroy #Destroy old token
                 end
             end
             if(!sms)
@@ -29,6 +30,7 @@ class SuperToken < ApplicationRecord
                 # generate token based off user
                 SuperToken.create!(token:hash, user_id: user.id, client_ip: request.remote_ip, agent: request.user_agent, expiry: Time.now)
             else
+                SuperToken.where(user_id:user.id).where(is_sms:true).destroy_all #Destroys all the old sms tokens to create a new one
                 randomNumber = rand(12345..99999)
                 SuperToken.create!(token:"#{user.id}#{randomNumber}",is_sms:true, user_id: user.id, client_ip: request.remote_ip, agent: request.user_agent, expiry: Time.now)
             end
@@ -46,9 +48,14 @@ class SuperToken < ApplicationRecord
             return {status: "bad", error:"SuperToken Incorrect", message:"Token doesnt exist in database"}
         end
         if super_token.is_sms
+            age_of_token = Time.now.to_i - super_token.created_at.to_i 
             user = super_token.user
             super_token.destroy
-            return {status: "ok", user:user}
+            if(age_of_token < SMS_EXPIRY*60)
+                return {status: "ok", user:user}
+            else
+                return {status: "bad", error:"401 not authorized", message:"EXPIRED SMS TOKEN"}
+            end
         end
         if super_token.client_ip == request.remote_ip
             if is_expired super_token.expiry.to_i
